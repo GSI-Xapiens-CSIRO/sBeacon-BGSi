c614ac93b8fe2985939c5129c716e574ae24d4e8
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-12-03 23:16:47 +0000

    MAINT: clean up unused DynamoDB table definitions in dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,31 +1,6 @@
-# resource "aws_dynamodb_table" "datasets" {
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "id"
-#   name         = "sbeacon-Datasets"
-#   tags         = var.common-tags
-
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
-
-#   attribute {
-#     name = "assemblyId"
-#     type = "S"
-#   }
-
-#   global_secondary_index {
-#     hash_key = "assemblyId"
-#     name     = "assembly_index"
-#     non_key_attributes = [
-#       "id",
-#       "vcfLocations",
-#       "vcfGroups",
-#       "vcfChromosomeMap"
-#     ]
-#     projection_type = "INCLUDE"
-#   }
-# }
+# 
+# sBeacon DynamoDB Tables
+# 
 
 resource "aws_dynamodb_table" "ontologies" {
   billing_mode = "PAY_PER_REQUEST"
@@ -62,61 +37,6 @@ resource "aws_dynamodb_table" "anscestor_terms" {
     type = "S"
   }
 }
-
-# # this table holds the query made by user
-# # this is used to control the lambdas that
-# # execute a given query
-# # resonses counts the completed lambdas
-# resource "aws_dynamodb_table" "variant_queries" {
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "id"
-#   name         = "sbeacon-VariantQueries"
-#   tags         = var.common-tags
-
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
-
-#   # enable this later to save cost
-#   ttl {
-#     attribute_name = "timeToExist"
-#     enabled        = true
-#   }
-# }
-
-# # this table holds responses by perform query operation
-# # responseIndex is used to order and paginate
-# # this points to a JSON files with results
-# resource "aws_dynamodb_table" "variant_query_responses" {
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "id"
-#   range_key    = "responseNumber"
-#   name         = "sbeacon-VariantQueryResponses"
-#   tags         = var.common-tags
-
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
-
-#   attribute {
-#     name = "responseNumber"
-#     type = "N"
-#   }
-
-#   local_secondary_index {
-#     range_key       = "responseNumber"
-#     name            = "responseNumber_index"
-#     projection_type = "ALL"
-#   }
-
-#   # enable this later to save cost
-#   ttl {
-#     attribute_name = "timeToExist"
-#     enabled        = true
-#   }
-# }
 
 # 
 # Data Portal Tables

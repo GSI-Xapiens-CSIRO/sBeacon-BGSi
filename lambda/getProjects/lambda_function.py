d2ba2b151efa83fc476b8fc5c498fc36f4f49e52
NickEdwards7502 <nickedwards7502@gmail.com>
2024-12-06 02:49:51 +0000

    DEV: Restrict exposed project information to required fields

--- lambda/getProjects/lambda_function.py
+++ lambda/getProjects/lambda_function.py
@@ -7,6 +7,13 @@ from shared.apiutils import bundle_response
 def lambda_handler(event, context):
     print("Event Received: {}".format(json.dumps(event)))
     projects = Projects.scan()
-    response = [project.to_dict() for project in projects]
+    response = [
+        {
+            "name": project.name,
+            "description": project.description,
+            "total_samples": project.total_samples
+        }
+        for project in projects
+    ]
     print("Returning Response: {}".format(json.dumps(response)))
     return bundle_response(200, response)

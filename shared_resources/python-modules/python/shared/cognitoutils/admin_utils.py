34adaced650d7f0a692852c556350309f47d68c5
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-12-10 06:58:15 +0000

    FIX: bug not validating admins and managers

--- shared_resources/python-modules/python/shared/cognitoutils/admin_utils.py
+++ shared_resources/python-modules/python/shared/cognitoutils/admin_utils.py
@@ -17,7 +17,10 @@ def authenticate_manager(event, context):
     authorizer = event["requestContext"]["authorizer"]
     groups = authorizer["claims"]["cognito:groups"].split(",")
 
-    if not ENV_COGNITO.COGNITO_MANAGER_GROUP_NAME in groups:
+    if not (
+        ENV_COGNITO.COGNITO_MANAGER_GROUP_NAME in groups
+        or ENV_COGNITO.COGNITO_ADMIN_GROUP_NAME in groups
+    ):
         raise AuthError(
             error_code="Unauthorised",
             error_message="User does not have management access",

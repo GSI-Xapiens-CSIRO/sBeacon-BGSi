8efe68a9f792c8859267e1ab0aa6cf5927feefe8
NickEdwards7502 <nickedwards7502@gmail.com>
2025-06-04 00:47:39 +0000

    FIX: Define clinic regions bucket variables

--- variables.tf
+++ variables.tf
@@ -276,3 +276,13 @@ variable "clinic-temp-bucket-arn" {
   type        = string
   description = "ARN of the clinic temp bucket"
 }
+
+variable "clinic-region-bucket-name" {
+  type        = string
+  description = "Name of the clinic regions bucket"
+}
+
+variable "clinic-region-bucket-arn" {
+  type        = string
+  description = "ARN of the clinic regions bucket"
+}

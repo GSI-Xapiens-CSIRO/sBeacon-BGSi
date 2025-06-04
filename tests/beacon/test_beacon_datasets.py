073d474f4eaf6627eea7955d00e5cc41110f62d1
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2025-06-04 03:45:49 +0000

    TSTS: Add test suite for beacon and dataportal functionalities

---
+++ tests/beacon/test_beacon_datasets.py
@@ -0,0 +1,45 @@
+import json
+import os
+import sys
+from unittest.mock import patch
+
+import requests
+import boto3
+import pytest
+from moto import mock_aws
+
+
+@pytest.mark.dependency(name="test_create_notebook")
+def test_create_notebook(resources_dict):
+    import lambda_function
+
+    event = {
+        "resource": "/datasets",
+        "path": "/datasets",
+        "httpMethod": "POST",
+        "requestContext": {
+            "authorizer": {
+                "claims": {
+                    "sub": resources_dict["guest_sub"],
+                    "cognito:groups": "",
+                }
+            },
+        },
+        "body": json.dumps(
+            {
+                "projects": ["ci_cd_project"],
+                "query": {
+                    "filters": [],
+                    "requestedGranularity": "record",
+                    "pagination": {"skip": 0, "limit": 10},
+                },
+                "meta": {"apiVersion": "v2.0"},
+            }
+        ),
+    }
+
+    response = lambda_function.lambda_handler(event, {})
+    body = json.loads(response["body"])
+
+    assert body["responseSummary"]["exists"] is True
+    assert body["responseSummary"]["numTotalResults"] == 2
--- tests/tests.sh
+++ tests/tests.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+
 pytest -p no:warnings -vv ./admin/
 pytest -p no:warnings -vv ./dataportal/
+pytest -p no:warnings -vv ./beacon/

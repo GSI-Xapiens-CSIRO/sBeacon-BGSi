d33f4995d6ca2f4009af8839f629b8c2fc6790e5
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2025-07-01 14:43:42 +0700

    fix: change table name

--- shared_resources/python-modules/python/shared/dynamodb/user_info.py
+++ shared_resources/python-modules/python/shared/dynamodb/user_info.py
@@ -14,7 +14,7 @@ REGION = SESSION.region_name
 
 class UserInfo(Model):
     class Meta:
-        table_name = ENV_DYNAMO.DYNAMO_QUOTA_USER_TABLE
+        table_name = ENV_DYNAMO.DYNAMO_USER_INFO_TABLE
         region = REGION
 
     uid = UnicodeAttribute(hash_key=True)

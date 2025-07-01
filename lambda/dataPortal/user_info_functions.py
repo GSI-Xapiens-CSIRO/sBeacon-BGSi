fa9efb28ee01bdf63ffb570a64fb997f7a8388de
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2025-07-01 14:57:16 +0700

    feat: update user lambda function

--- lambda/dataPortal/user_info_functions.py
+++ lambda/dataPortal/user_info_functions.py
@@ -8,41 +8,43 @@ router = LambdaRouter()
 
 @router.attach("/dportal/userinfo", "post")
 def store_user_info(event, context):
-    if isinstance(event["body"], str):
-        body = json.loads(event["body"])
-    else:
-        body = event["body"]
-
-    uid = body.get("uid")
-    institutionName = body.get("institutionName", "")
-
     try:
-        user_info = UserInfo.get(uid)
-        user_info.update(actions=[UserInfo.institutionName.set(institutionName)])
-    except UserInfo.DoesNotExist:
-        user_info = UserInfo(
-            uid=uid,
-            institutionName=institutionName,
-        )
-        user_info.save()
+        if isinstance(event["body"], str):
+            body = json.loads(event["body"])
+        else:
+            body = event["body"]
 
-    return user_info.to_dict()
+        uid = body.get("uid")
+        institutionName = body.get("institutionName", "")
+
+        if not uid:
+            return {"success": False, "message": "Missing 'uid' in request body."}
+
+        try:
+            user_info = UserInfo.get(uid)
+            user_info.update(actions=[UserInfo.institutionName.set(institutionName)])
+        except UserInfo.DoesNotExist:
+            user_info = UserInfo(
+                uid=uid,
+                institutionName=institutionName,
+            )
+            user_info.save()
+
+        return {"success": True, "message": ""}
+
+    except Exception as e:
+        return {"success": False, "message": f"An error occurred: {str(e)}"}
 
 
-@router.attach("/dportal/userinfo", "get")
+@router.attach("/dportal/userinfo/{uid}", "get")
 def get_user_info(event, context):
-    if isinstance(event["body"], str):
-        body = json.loads(event["body"])
-    else:
-        body = event["body"]
-
-    uid = body.get("uid")
+    uid = event["pathParameters"]["uid"]
 
     try:
         user_info = UserInfo.get(uid)
     except UserInfo.DoesNotExist:
         raise PortalError(
-            error_code=404,
+            error_code=409,
             error_message=f"User not found.",
         )
 

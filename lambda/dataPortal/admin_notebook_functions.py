93745c65de3abe88f15ced10595a716cf4a46cdd
NickEdwards7502 <nickedwards7502@gmail.com>
2025-07-01 07:49:01 +0000

    DEV: Server-side pagination for admin notebook list

--- lambda/dataPortal/admin_notebook_functions.py
+++ lambda/dataPortal/admin_notebook_functions.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import boto3
@@ -5,7 +6,7 @@ from pynamodb.exceptions import DeleteError
 
 from shared.apiutils import LambdaRouter, PortalError
 from utils.models import InstanceStatus, JupyterInstances
-from utils.cognito import get_user_from_attribute, get_user_attribute
+from utils.cognito import list_users, get_user_from_attribute, get_user_attribute
 from utils.sagemaker import list_all_notebooks
 from shared.cognitoutils import authenticate_manager
 
@@ -23,18 +24,49 @@ DPORTAL_BUCKET = os.environ.get("DPORTAL_BUCKET")
 
 @router.attach("/dportal/admin/notebooks", "get", authenticate_manager)
 def list_notebooks(event, context):
-    instances = {
-        f"{instance.instanceName}-{instance.uid}": instance
-        for instance in JupyterInstances.scan()
+    query_params = event.get("queryStringParameters", {})
+    if query_params:
+        limit = int(query_params.get("limit", 3))
+        last_evaluated_index = int(query_params.get("last_evaluated_index", 0))
+        search_term = query_params.get("search", None)
+
+    instances = JupyterInstances.scan()
+    if search_term:
+        search_term = search_term.lower()
+        users = list_users()
+        subs = {
+            user["sub"]
+            for user in users
+            if search_term in user.get("email", "").lower()
+            or search_term in user.get("given_name", "").lower()
+            or search_term in user.get("last_name", "").lower()
+        }
+        instances = [
+            instance
+            for instance in instances
+            if instance.uid in subs or search_term in instance.instanceName.lower()
+        ]
+
+    instance_dict = {
+        f"{instance.instanceName}-{instance.uid}": instance for instance in instances
     }
-    notebooks = list_all_notebooks()
+    sorted_instance_keys = sorted(instance_dict.keys())
+
+    total_count = len(sorted_instance_keys)
+    start_index = last_evaluated_index
+    end_index = min(start_index + limit, total_count)
+
+    paginated_instance_keys = sorted_instance_keys[start_index:end_index]
+    paginated_instance_dict = {
+        key: instance_dict[key] for key in paginated_instance_keys
+    }
+
+    notebooks = list_all_notebooks(paginated_instance_keys)
 
     for notebook in notebooks:
-        if InstanceStatus(notebook["status"]) == InstanceStatus.DELETING:
-            continue
         try:
             user = get_user_from_attribute(
-                "sub", instances[notebook["instanceName"]].uid
+                "sub", paginated_instance_dict[notebook["instanceName"]].uid
             )
             notebook["userFirstName"] = get_user_attribute(user, "given_name")
             notebook["userLastName"] = get_user_attribute(user, "family_name")
@@ -44,7 +76,15 @@ def list_notebooks(event, context):
             notebook["userLastName"] = "Unassigned"
             notebook["userEmail"] = "Unassigned"
 
-    return notebooks
+    last_evaluated_index = None if end_index >= total_count else end_index
+
+    response = {
+        "success": True,
+        "notebooks": notebooks,
+        "lastEvaluatedIndex": last_evaluated_index,
+    }
+    print(f"Returning response: {response}")
+    return response
 
 
 @router.attach("/dportal/admin/notebooks/{name}", "get", authenticate_manager)
--- lambda/dataPortal/utils/sagemaker.py
+++ lambda/dataPortal/utils/sagemaker.py
@@ -3,26 +3,26 @@ import boto3
 sagemaker_client = boto3.client("sagemaker")
 
 
-def list_all_notebooks():
+def list_all_notebooks(instance_ids=[]):
     response = sagemaker_client.list_notebook_instances()
     notebooks = response["NotebookInstances"]
-    statuses = [
-        sagemaker_client.describe_notebook_instance(
-            NotebookInstanceName=notebook["NotebookInstanceName"]
-        )
-        for notebook in notebooks
-    ]
 
     while "NextToken" in response:
         next_token = response["NextToken"]
         response = sagemaker_client.list_notebook_instances(NextToken=next_token)
         notebooks += response["NotebookInstances"]
-        statuses += [
-            sagemaker_client.describe_notebook_instance(
-                NotebookInstanceName=notebook["NotebookInstanceName"]
-            )
-            for notebook in notebooks
-        ]
+
+    paginated_notebooks = [
+        notebook
+        for notebook in notebooks
+        if notebook["NotebookInstanceName"] in instance_ids
+    ]
+    statuses = [
+        sagemaker_client.describe_notebook_instance(
+            NotebookInstanceName=notebook["NotebookInstanceName"]
+        )
+        for notebook in paginated_notebooks
+    ]
 
     return [
         {
@@ -31,5 +31,5 @@ def list_all_notebooks():
             "instanceType": notebook["InstanceType"],
             "volumeSize": status["VolumeSizeInGB"],
         }
-        for (notebook, status) in zip(notebooks, statuses)
+        for (notebook, status) in zip(paginated_notebooks, statuses)
     ]

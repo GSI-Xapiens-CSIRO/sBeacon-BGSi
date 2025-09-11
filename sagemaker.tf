9473f0664f08fa318d7365749c9a856b2f19a7f1
Titus Adi Prasetyo <Titus.Prasetyo@xapiens.id>
2025-09-11 10:45:42 +0700

    add disable_download

--- sagemaker.tf
+++ sagemaker.tf
@@ -24,20 +24,6 @@ sudo unlink /home/ec2-user/sample-notebooks
 git clone --depth 1 https://github.com/GSI-Xapiens-CSIRO/GASPI-ETL-notebooks.git /home/ec2-user/GASPI-ETL-notebooks
 sudo ln -s /home/ec2-user/GASPI-ETL-notebooks /home/ec2-user/sample-notebooks
 
-source /etc/profile.d/conda.sh
-conda activate python3
-
-# Install JupyterLab extensions jika belum (SageMaker sudah punya, tapi pastikan)
-pip install jupyterlab
-
-# Disable download extensions
-jupyter labextension disable @jupyterlab/docmanager-extension:download
-jupyter labextension disable @jupyterlab/filebrowser-extension:download
-
-# Restart Jupyter server untuk apply changes
-sudo /home/ec2-user/anaconda3/envs/python3/bin/jupyter lab build
-sudo systemctl restart jupyter
-
 EOT
   )
 
@@ -49,12 +35,79 @@ sudo unlink /home/ec2-user/sample-notebooks
 git clone --depth 1 https://github.com/GSI-Xapiens-CSIRO/GASPI-ETL-notebooks.git /home/ec2-user/GASPI-ETL-notebooks
 sudo ln -s /home/ec2-user/GASPI-ETL-notebooks /home/ec2-user/sample-notebooks
 
-source /etc/profile.d/conda.sh
-conda activate python3
-jupyter labextension disable @jupyterlab/docmanager-extension:download
-jupyter labextension disable @jupyterlab/filebrowser-extension:download
-sudo systemctl restart jupyter
-
 EOT
+  )
+}
+
+# Lifecycle Configuration: Disable download menu untuk JupyterLab 4
+resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "disable_download" {
+  name = "disable-download-config"
+
+  on_create = base64encode(<<-EOT
+    #!/bin/bash
+    # Pastikan direktori konfigurasi Jupyter ada
+    mkdir -p /home/ec2-user/.jupyter
+
+    # Tambahkan konfigurasi server untuk menonaktifkan download
+    cat << EOF > /home/ec2-user/.jupyter/jupyter_server_config.json
+    {
+      "ServerApp": {
+        "jpserver_extensions": {
+          "jupyterlab": true
+        },
+        "custom_display_url": "/lab"
+      },
+      "LabApp": {
+        "disabled_extensions": ["@jupyterlab/docmanager-extension:download", "@jupyterlab/filebrowser-extension:download"]
+      }
+    }
+    EOF
+
+    # Tambahkan custom JavaScript untuk override download UI
+    mkdir -p /home/ec2-user/.jupyter/custom
+    cat << EOF > /home/ec2-user/.jupyter/custom/custom.js
+    define(['base/js/namespace'], function(Jupyter) {
+      Jupyter.notebook_list.actions.remove('download-notebook');
+      Jupyter.filebrowser.actions.remove('download-file');
+    });
+    EOF
+
+    # Pastikan izin file benar
+    chown ec2-user:ec2-user /home/ec2-user/.jupyter -R
+
+    # Restart Jupyter server untuk apply changes
+    sudo systemctl restart jupyter-server
+    EOT
+  )
+
+  on_start = base64encode(<<-EOT
+    #!/bin/bash
+    # Pastikan konfigurasi tetap ada saat start
+    mkdir -p /home/ec2-user/.jupyter
+    cat << EOF > /home/ec2-user/.jupyter/jupyter_server_config.json
+    {
+      "ServerApp": {
+        "jpserver_extensions": {
+          "jupyterlab": true
+        },
+        "custom_display_url": "/lab"
+      },
+      "LabApp": {
+        "disabled_extensions": ["@jupyterlab/docmanager-extension:download", "@jupyterlab/filebrowser-extension:download"]
+      }
+    }
+    EOF
+
+    mkdir -p /home/ec2-user/.jupyter/custom
+    cat << EOF > /home/ec2-user/.jupyter/custom/custom.js
+    define(['base/js/namespace'], function(Jupyter) {
+      Jupyter.notebook_list.actions.remove('download-notebook');
+      Jupyter.filebrowser.actions.remove('download-file');
+    });
+    EOF
+
+    chown ec2-user:ec2-user /home/ec2-user/.jupyter -R
+    sudo systemctl restart jupyter-server
+    EOT
   )
 }

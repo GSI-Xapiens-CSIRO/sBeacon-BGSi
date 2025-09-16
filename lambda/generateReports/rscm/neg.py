39b01ea9b1f87db1d6f105f6288fe0343e2f4a9a
sahmad <syaiful.ahmad@xapiens.id>
2025-09-16 09:42:55 +0700

    feat: enhance report generation with user data and clean up code

--- lambda/generateReports/rscm/neg.py
+++ lambda/generateReports/rscm/neg.py
@@ -1,6 +1,6 @@
 import os
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 from PyPDF2 import PdfReader, PdfWriter
@@ -8,6 +8,10 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
 
+def clear_area(c, x, y, width=200, height=15, color=colors.white):
+    c.setFillColor(color)
+    c.rect(x, y, width, height, fill=1, stroke=0)
+    c.setFillColor(colors.black)
 
 def _create_annotations(
     date_pos,
@@ -16,19 +20,43 @@ def _create_annotations(
     rekam_medis_pos,
     gender_pos,
     symptoms_pos,
+    clinical_diagnosis_pos,
+    physician_pos,
+    genetic_counselor_pos,
     footer_name_pos,
     footer_dob_pos,
     filename,
     versions,
     report_id,
+    validated_at,
+    validated_by,
+    project_value,
+    vcf_value,
+    validated_comment,
+    qc_note
 ):
     c = canvas.Canvas(filename, pagesize=letter)
     form = c.acroForm
+    text_color = colors.HexColor("#156082")
 
-    # date
-    x, y, fs, text = date_pos
-    c.setFont("Helvetica-Bold", fs)
-    c.drawString(x, y, text)
+    print("validated_comment:", validated_comment)
+    # Clinical Diagnosis
+    x, y, fs, text = clinical_diagnosis_pos
+    c.setFont("Helvetica", fs)
+    c.setFillColor(colors.black)
+    c.drawString(x, y, text or "")
+
+    # Physician
+    x, y, fs, text = physician_pos
+    c.setFont("Helvetica", fs)
+    c.setFillColor(colors.black)
+    c.drawString(x, y, text or "")
+
+    # Genetic Counselor
+    x, y, fs, text = genetic_counselor_pos
+    c.setFont("Helvetica", fs)
+    c.setFillColor(colors.black)
+    c.drawString(x, y, text or "")
 
     # name
     x, y, fs, text = name_pos
@@ -38,9 +66,9 @@ def _create_annotations(
         value=f"{text}",
         x=x,
         y=y,
-        width=200,
-        height=12,
-        fontSize=8,
+        width=220,
+        height=13,
+        fontSize=fs,
         borderWidth=0,
         fillColor=colors.white,
         textColor=None,
@@ -48,7 +76,7 @@ def _create_annotations(
         fieldFlags=0,
     )
 
-    # dob
+    #dob
     x, y, fs, text = dob_pos
     form.textfield(
         name="dob",
@@ -56,9 +84,9 @@ def _create_annotations(
         value=f"{text}",
         x=x,
         y=y,
-        width=100,
-        height=12,
-        fontSize=8,
+        width=220,
+        height=13,
+        fontSize=fs,
         borderWidth=0,
         fillColor=colors.white,
         textColor=None,
@@ -71,12 +99,12 @@ def _create_annotations(
     form.textfield(
         name="rekam",
         tooltip="Rekam Meidis",
-        value="",
+        value=text,
         x=x,
         y=y,
-        width=200,
-        height=12,
-        fontSize=8,
+        width=220,
+        height=13,
+        fontSize=fs,
         borderWidth=0,
         fillColor=colors.white,
         textColor=None,
@@ -92,28 +120,11 @@ def _create_annotations(
         value=text,
         x=x,
         y=y,
-        width=100,
+        width=220,
         height=13,
         fontName="Helvetica",
-        fontSize=8,
-        options=[("Male", "male"), ("Female", "female")],
-        borderWidth=0,
-        fillColor=colors.white,
-        textColor=None,
-        forceBorder=False,
-    )
-
-    # symptoms
-    x, y, fs, text = symptoms_pos
-    form.textfield(
-        name="symptoms",
-        tooltip="Symptoms",
-        value="",
-        x=x,
-        y=y,
-        width=200,
-        height=12,
-        fontSize=8,
+        fontSize=fs,
+        options=["Male", "Female"],
         borderWidth=0,
         fillColor=colors.white,
         textColor=None,
@@ -121,10 +132,29 @@ def _create_annotations(
         fieldFlags=0,
     )
 
-    for n in range(3):
+    # symptoms
+    x, y, fs, text = symptoms_pos
+    form.textfield(
+        name="symptoms",
+        tooltip="Symptoms",
+        value=text,
+        x=x,
+        y=y,
+        width=220,
+        height=13,
+        fontSize=fs,
+        fieldFlags=0,
+        borderWidth=0,
+        fillColor=colors.white,
+        textColor=None,
+        forceBorder=False,
+    )
+
+    for n in range(4):
         x, y, fs, text = (5, 780, 12, report_id)
         c.setFont("Helvetica", fs)
         c.drawString(x, y, text)
+        
         # footer name
         x, y, fs, text = footer_name_pos
         form.textfield(
@@ -133,12 +163,12 @@ def _create_annotations(
             value=f"{text}",
             x=x,
             y=y,
-            width=200,
-            height=12,
-            fontSize=8,
+            width=160,
+            height=11,
+            fontSize=fs,
             borderWidth=0,
             fillColor=colors.white,
-            textColor=None,
+            textColor=text_color,
             forceBorder=False,
             fieldFlags=0,
         )
@@ -151,52 +181,75 @@ def _create_annotations(
             value=f"{text}",
             x=x,
             y=y,
-            width=100,
-            height=12,
-            fontSize=8,
+            width=160,
+            height=11,
+            fontSize=fs,
             borderWidth=0,
             fillColor=colors.white,
-            textColor=None,
+            textColor=text_color,
             forceBorder=False,
             fieldFlags=0,
         )
+
+        # footer date
+        x, y, fs, text = date_pos
+        c.setFont("Helvetica", fs)
+        c.setFillColor(text_color)
+        c.drawString(x, y, text or "")
+        
         if n == 2:
             versions_pos = [
                 # left col
-                (180, 570 + 15, 11, versions["snp_eff_version"]),
-                (180, 556 + 15, 11, versions["snp_sift_version"]),
-                (180, 542 + 15, 11, versions["clinvar_version"]),
-                (180, 528 + 15, 11, versions["omim_version"]),
+                (180, 570 + 15, 11, versions["clinvar_version"]),
+                (180, 556 + 15, 11, versions["gnomad_version"]),
                 # right col
-                (450 - 20, 570 + 15, 11, versions["gnomad_version"]),
-                (450 - 20, 556 + 15, 11, versions["dbsnp_version"]),
-                (450 - 20, 542 + 15, 11, versions["sift_version"]),
-                (450 - 20, 528 + 15, 11, versions["polyphen2_version"]),
+                (450 - 20, 570 + 15, 11, versions["dbsnp_version"]),
+                (450 - 20, 556 + 15, 11, versions["sift_version"]),
             ]
             for pos in versions_pos:
                 x, y, fs, text = pos
                 c.setFont("Helvetica", fs)
+                c.setFillColor(colors.black)
                 c.drawString(x, y, text)
-            # Clinical Notes
-            c.setFont("Helvetica-Bold", 13)
-            c.setFillColor(colors.HexColor("#156082"))
-            c.drawString(70, 500, f"Clinical Notes")
-            x, y, fs, text = (70, 400, 12, "")
-            form.textfield(
-                name="clinical_notes",
-                tooltip="",
-                value=f"{text}",
-                x=x,
-                y=y,
-                width=463,
-                height=90,
-                fontSize=8,
-                borderWidth=0,
-                fillColor=colors.white,
-                textColor=None,
-                forceBorder=False,
-                fieldFlags=1<<12,
-            )
+            
+            x, y, fs, text = validated_at
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            x, y, fs, text = validated_by
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+        if n == 3:
+            #project
+            x, y, fs, text = project_value
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            #vcf
+            x, y, fs, text = vcf_value
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            x, y, fs, text = validated_comment
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            x, y, fs, text = qc_note
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+        # page num
+        c.setFont("Helvetica", 10)
+        c.setFillColor(colors.HexColor("#156082"))
+        c.drawString(490, 68, f"Page {n+1} of 4")
+
         c.showPage()
 
     c.save()
@@ -223,22 +276,61 @@ def _overlay_pdf_with_annotations(src, dest, output):
         writer.write(f)
 
 
-def generate(
-    *, pii_name=None, pii_dob=None, pii_gender=None, versions=None, report_id=None
+def generate_neg(
+    *,
+    pii_name=None,
+    pii_dob=None,
+    pii_gender=None,
+    pii_rekam_medis=None,
+    pii_clinical_diagnosis=None,
+    pii_symptoms=None,
+    pii_physician=None,
+    pii_genetic_counselor=None,
+    variants=None,
+    versions=None,
+    report_id=None,
+    project=None,   
+    vcf=None,
+    user=None,
+    validated_at=None,
+    validated_comment=None,
+    qc_note=None
 ):
     module_dir = Path(__file__).parent
     output_pdf_path = "/tmp/annotations.pdf"
     input_pdf_path = f"{module_dir}/neg.pdf"
 
     # x, y, h, text
-    date_pos = (72, 595, 12, f"Date: {datetime.now().strftime('%d %B %Y')}")
-    name_pos = (192, 564, 12, pii_name)
-    dob_pos = (192, 550, 12, pii_dob)
-    rekam_medis_pos = (192, 538, 12, f"")
-    gender_pos = (192, 524, 12, pii_gender)
-    symptoms_pos = (192, 492, 12, f"")
-    footer_name_pos = (146, 48, 12, pii_name)
-    footer_dob_pos = (146, 36, 12, pii_dob)
+    date_pos = (158, 47, 10, datetime.now().strftime('%Y-%m-%d'))
+    name_pos = (192, 566, 12, "")
+    dob_pos = (192, 552, 12, "")
+    rekam_medis_pos = (192, 538, 12, "")
+    gender_pos = (192, 524, 12, "Male")
+    clinical_diagnosis_pos = (192, 510, 12, "Familial Hypercholesterolemia (FH)")
+    symptoms_pos = (192, 494, 12, "")
+    physician_pos = (192, 480, 12, "dr. Dicky Tahapary, SpPD-KEMD., PhD")
+    genetic_counselor_pos = (192, 464, 12, "dr. Widya Eka Nugraha, M.Si. Med.")
+    
+    footer_name_pos = (148, 72, 10, "")
+    footer_dob_pos = (148, 58, 10, "")
+    footer_date_pos = (158, 47, 10, datetime.now().strftime('%Y-%m-%d'))
+    
+    project_value = (72, 505, 12, project or "")
+    vcf_value = (72, 550, 12, vcf or "")
+    validated_by = (205, 175, 11, f"{user.get('firstName', '')} {user.get('lastName', '')}".strip())
+
+    try:
+        dt = datetime.fromisoformat(validated_at)
+        dt_wib = dt + timedelta(hours=7)
+        validated_at_str = dt_wib.strftime("%Y-%m-%d %H:%M")
+    except Exception as e:
+        print("Parse error:", e)
+        validated_at_str = validated_at
+
+    validated_at = (205, 161, 11, validated_at_str)
+    validated_comment= (72, 442, 11, validated_comment or "")
+
+    qc_note = (72, 280, 12, qc_note)
 
     _create_annotations(
         date_pos,
@@ -247,11 +339,20 @@ def generate(
         rekam_medis_pos,
         gender_pos,
         symptoms_pos,
+        clinical_diagnosis_pos,
+        physician_pos,
+        genetic_counselor_pos,
         footer_name_pos,
         footer_dob_pos,
         output_pdf_path,
         versions,
         report_id,
+        validated_at,
+        validated_by,
+        project_value,
+        vcf_value,
+        validated_comment,
+        qc_note
     )
     output_file_name = f"/tmp/{str(uuid.uuid4())}.pdf"
     _overlay_pdf_with_annotations(output_pdf_path, input_pdf_path, output_file_name)
@@ -262,7 +363,7 @@ def generate(
 
 
 if __name__ == "__main__":
-    generate(
+    generate_neg(
         pii_name="John Doe",
         pii_dob="01/01/1990",
         pii_gender="Male",
--- lambda/generateReports/rscm/pos_stage_3.py
+++ lambda/generateReports/rscm/pos_stage_3.py
@@ -5,18 +5,76 @@ from PyPDF2 import PdfReader, PdfWriter
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
+from datetime import datetime, timedelta
 
 
 def _create_annotations(
-    filename, pages, footer_name_pos, footer_dob_pos, page_num_pos, report_id=None
+    filename,
+    pages,
+    footer_name_pos,
+    footer_dob_pos,
+    footer_date_pos,
+    page_num_pos,
+    report_id=None,
+    versions_pos=None,
+    validated_at=None,
+    validated_by=None,
+    project_value=None,
+    vcf_value=None,
+    result_annotation=None,
+    qc_note=None
 ):
     c = canvas.Canvas(filename, pagesize=letter)
+    text_color = colors.HexColor("#156082")
     form = c.acroForm
-
+    
     for p in range(pages):
-        x, y, fs, text = (5, 780, 12, report_id)
+        # report_id di header
+        x, y, fs, text = (5, 778, 11, report_id or "")
         c.setFont("Helvetica", fs)
         c.drawString(x, y, text)
+
+        if p == pages - 2:
+            for pos in versions_pos:
+                x, y, fs, text = pos
+                c.setFont("Helvetica", fs)
+                c.drawString(x, y, text or "")
+            
+            x, y, fs, text = validated_at
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            x, y, fs, text = validated_by
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+        if p == pages - 1:
+            #project
+            x, y, fs, text = project_value
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            #vcf
+            x, y, fs, text = vcf_value
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            #qc_notes
+            x, y, fs, text = qc_note
+            c.setFont("Helvetica", fs)
+            c.setFillColor(colors.black)
+            c.drawString(x, y, text or "")
+
+            for ra in result_annotation:
+                x, y, fs, text = ra
+                c.setFont("Helvetica", fs)
+                c.setFillColor(colors.black)
+                c.drawString(x, y, text or "")
+
         # footer name
         x, y, fs, text = footer_name_pos
         form.textfield(
@@ -25,12 +83,12 @@ def _create_annotations(
             value=f"{text}",
             x=x,
             y=y,
-            width=200,
-            height=12,
-            fontSize=8,
+            width=160,
+            height=11,
+            fontSize=fs,
             borderWidth=0,
             fillColor=colors.white,
-            textColor=None,
+            textColor=text_color,
             forceBorder=False,
             fieldFlags=0,
         )
@@ -43,32 +101,87 @@ def _create_annotations(
             value=f"{text}",
             x=x,
             y=y,
-            width=100,
-            height=12,
-            fontSize=8,
+            width=160,
+            height=11,
+            fontSize=fs,
             borderWidth=0,
             fillColor=colors.white,
-            textColor=None,
+            textColor=text_color,
             forceBorder=False,
             fieldFlags=0,
         )
 
+        # footer date
+        x, y, fs, text = footer_date_pos
+        c.setFont("Helvetica", fs)
+        c.setFillColor(text_color)
+        c.drawString(x, y, text or "")
+
         # page num
         x, y = page_num_pos
-        c.setFont("Helvetica", fs)
+        c.setFont("Helvetica", 10)
         c.setFillColor(colors.HexColor("#156082"))
         c.drawString(x, y, f"Page {p+1} of {pages}")
+
         c.showPage()
     c.save()
 
 
 def generate(
-    summary_pdf, results_pdf, annots_pdf, *, pii_name=None, pii_dob=None, report_id=None
+    summary_pdf,
+    results_pdf,
+    annots_pdf,
+    *,
+    pii_name=None,
+    pii_dob=None,
+    report_id=None,
+    versions=None,
+    variant_validations=None,
+    project=None,
+    vcf=None,
+    user=None,
+    qc_note=None
 ):
+    validated_by, validated_at = None, None
+    result_annotation = []
+
+    if variant_validations:
+
+        result_annotation = [
+            (72, (442 - i*13), 11, v.get("validationComment", ""))
+            for i, v in enumerate(variant_validations)
+            if v.get("validationComment")
+        ]
+        
+        latest_validation = max(
+            variant_validations,
+            key=lambda v: v.get("validatedAt", "")
+        )
+
+        #validated_by
+        user = latest_validation.get("user", {})
+        full_name = f"{user.get('firstName', '')} {user.get('lastName', '')}".strip()
+        validated_by = (205, 150, 11, full_name)
+        
+        #validated_at
+        raw_validated_at = latest_validation.get("validatedAt")
+        validated_at_str = ""
+        if raw_validated_at:
+            try:
+                dt = datetime.fromisoformat(raw_validated_at)
+                dt_local = dt + timedelta(hours=7)
+                validated_at_str = dt_local.strftime("%Y-%m-%d %H:%M")
+            except Exception as e:
+                validated_at_str = str(raw_validated_at)
+
+        validated_at = (205, 136, 11, validated_at_str)
+
+
     # x, y, fs, text
-    footer_name_pos = (146, 48, 12, pii_name)
-    footer_dob_pos = (146, 36, 12, pii_dob)
-    page_num_pos = (500, 70)
+    footer_name_pos = (148, 72, 10, "")
+    footer_dob_pos = (148, 58, 10, "")
+    footer_date_pos = (158, 47, 10, datetime.now().strftime('%Y-%m-%d'))
+    page_num_pos = (490, 70)
 
     output_pdf_path = "/tmp/annotations.pdf"
     pdf_int = PdfReader(annots_pdf)
@@ -77,24 +190,46 @@ def generate(
 
     total_pages = len(pdf_int.pages) + len(pdf_results.pages) + len(pdf_summary.pages)
 
+    versions_pos = [
+        (184, 353, 11, versions["clinvar_version"]),
+        (184, 339, 11, versions["gnomad_version"]),
+        (184, 325, 11, versions["dbsnp_version"]),
+        (184, 311, 11, versions["sift_version"]),
+    ]
+
+    vcf_value = (72, 550, 12, vcf)
+    project_value = (72, 505, 12, project)
+
+    qc_note = (72, 240, 12, qc_note)
+
     _create_annotations(
         output_pdf_path,
         total_pages,
         footer_name_pos,
         footer_dob_pos,
+        footer_date_pos,
         page_num_pos,
         report_id,
+        versions_pos,
+        validated_at,
+        validated_by,
+        project_value,
+        vcf_value,
+        result_annotation,
+        qc_note
     )
 
     writer = PdfWriter()
     pages = []
 
+    # urutan pages
     pages.append(pdf_int.pages[0])
     pages += pdf_summary.pages
     pages.append(pdf_int.pages[1])
     pages += pdf_results.pages
     pages += pdf_int.pages[2:]
 
+    # merge annotations (footer & pagenum)
     footer_pagenum_annotations = PdfReader(output_pdf_path)
 
     for n, page in enumerate(pages):
